2f5ce36a1dc87bfe3a16ae88054027e8c4e49ebe
trupti <truptisharma105@gmail.com>
2023-02-01 19:36:29 +1300

    global class added

--- Mars/StepDefinitions/MarsStepDefinitions.cs
+++ Mars/StepDefinitions/MarsStepDefinitions.cs
@@ -2,10 +2,7 @@
 
 
 
-using Mars.Pages;
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+
 
 namespace Mars.StepDefinations
 {
---
+++ Mars/Utilities/Global.cs
@@ -0,0 +1,16 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Mars.Pages;
+global using NUnit.Framework;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Chrome;
+
+namespace Mars.Utilities
+{
+    internal class Global
+    {
+    }
+}
